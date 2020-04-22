8f33dfcab5cc599505a95c2c5ec599f2f6b477b4
github username <github email address>
2020-04-22 19:48:17 -0400

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,10 @@
+# has a title (FAILED - 1)
+#   belongs to an author (FAILED - 2)
+#   knows the name of the author it belongs to (FAILED - 3)
+
+class Post
+    attr_accessor :title, :author
+    def initialize
+    end
+
+end
